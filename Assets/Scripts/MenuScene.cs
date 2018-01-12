d1df1e981614b590f66037a484c3ffb77844c4d0
Viraj Malaka <mahanama.vm@gmail.com>
2018-01-12 20:26:18 +0530

    initial commit

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f;
+
+    private void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+    }
+
+    private void Update()
+    {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    }
+
+    public void OnPlayClick()
+    {
+        Debug.Log("Play Button");
+    }
+
+    public void OnAboutClick()
+    {
+        Debug.Log("about Button");
+    }
+}
